977ff2ff2253c3cd48dbfb47a5528941fe651939
Goutham-AR <argouthu8400@gmail.com>
2025-02-05 00:54:22 +0530

    update

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -2,6 +2,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>w", "<cmd>:w<CR>")
+vim.keymap.set("n", "<leader>q", "<cmd>:qa<CR>")
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", "<Tab>", "<cmd>:bn<CR>")
 vim.keymap.set("n", "<S-Tab>", "<cmd>:bp<CR>")
