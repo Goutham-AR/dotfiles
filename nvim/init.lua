b947eb14805bfc994801e878e4e32aca4a4e6372
Goutham-AR <argouthu8400@gmail.com>
2025-06-18 13:50:19 +0530

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -415,7 +415,8 @@ require("lazy").setup({
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = "prefer_rust_with_warning" },
+      -- fuzzy = { implementation = "prefer_rust_with_warning" },
+      fuzzy = { implementation = "lua" },
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
