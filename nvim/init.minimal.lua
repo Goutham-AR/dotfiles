dd2a16ece263b992bef312923d89c37bbf9a3295
Goutham-AR <argouthu8400@gmail.com>
2026-03-16 21:42:38 +0530

    update

---
+++ nvim/init.minimal.lua
@@ -0,0 +1,62 @@
+vim.opt.number         = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn     = "yes"
+vim.opt.scrolloff      = 8
+vim.opt.updatetime     = 150
+vim.opt.hlsearch   = true       -- highlight all matches
+vim.opt.incsearch  = true       -- highlight incrementally as you type
+vim.opt.ignorecase = true       -- case insensitive search
+vim.opt.smartcase  = true       -- override ignorecase if pattern has uppercase
+vim.opt.inccommand = "nosplit"  -- live preview of :s substitutions (neovim only)
+
+-- set colorscheme to habamax
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("habamax")
+
+vim.opt.path:append("**")
+vim.opt.wildoptions:append("fuzzy")
+vim.opt.wildignore:append({ "**/node_modules/**", "**/.git/**", "**/build/**", "**/dist/**" , "**/.venv/**" })
+
+-- Keymaps 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("n", "<leader>P", '"+P')
+vim.keymap.set("n", "<leader>c", "gcc", { remap = true })
+vim.keymap.set("v", "<leader>c", "gc", { remap = true })
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    error("Error cloning lazy.nvim:\n" .. out)
+  end
+end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+require("lazy").setup({
+  require("plugins.tmuxnavigator"),
+  require("plugins.oil"),
+  require("plugins.undotree"),
+})
+
+
+
