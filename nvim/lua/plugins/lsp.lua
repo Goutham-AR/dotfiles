9cc8688cd36897a66222feb14163b7bcce6caf0b
Goutham-AR <argouthu8400@gmail.com>
2026-03-31 21:06:49 +0530

    update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,12 +7,11 @@ return {
     "saghen/blink.cmp",
   },
   config = function()
-    -- local builtin = require("telescope.builtin")
-
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
       callback = function(event)
         local client = vim.lsp.get_client_by_id(event.data.client_id)
+        local fzf = require("fzf-lua")
         local map = function(keys, func, desc, mode)
           mode = mode or "n"
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
@@ -23,35 +22,17 @@ return {
         map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
         map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition", { "n", "x" })
 
-        -- Using native Neovim LSP functions (quickfix list) instead of Telescope pickers
-        -- map("grr", builtin.lsp_references, "[G]oto [R]eferences")
-        map("grr", vim.lsp.buf.references, "[G]oto [R]eferences")
-
-        -- map("gri", builtin.lsp_implementations, "[G]oto [I]mplementation")
-        map("gri", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-
-        -- map("grd", builtin.lsp_definitions, "[G]oto [D]efinition")
-        map("grd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+        map("grr", fzf.lsp_references, "[G]oto [R]eferences")
+        map("gri", fzf.lsp_implementations, "[G]oto [I]mplementation")
+        map("grd", fzf.lsp_definitions, "[G]oto [D]efinition")
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
         map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-        -- Find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        -- map("<leader>ds", builtin.lsp_document_symbols, "Open Document Symbols")
-        map("<leader>ds", vim.lsp.buf.document_symbol, "Open Document Symbols")
-
-        -- Find all the symbols in your current workspace.
-        --  Similar to document symbols, except searches over your entire project.
-        -- map("gW", builtin.lsp_workspace_symbols, "Open Workspace Symbols")
-        map("gW", vim.lsp.buf.workspace_symbol, "Open Workspace Symbols")
-
-        -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
-        -- map("grt", builtin.lsp_type_definitions, "[G]oto [T]ype Definition")
-        map("grt", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
+        map("<leader>ds", fzf.lsp_document_symbols, "Open Document Symbols")
+        map("gW", fzf.lsp_workspace_symbols, "Open Workspace Symbols")
+        map("grt", fzf.lsp_typedefs, "[G]oto [T]ype Definition")
 
         -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
         ---@param client vim.lsp.Client
