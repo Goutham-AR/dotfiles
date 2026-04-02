7fdc1a9dfc14488e37a3c0f167a1381ab72a07bb
Goutham-AR <argouthu8400@gmail.com>
2026-04-02 13:17:42 +0530

    update

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -36,16 +36,16 @@ vim.keymap.set("n", "g+", "<cmd>redo<CR>", { desc = "Redo" })
 vim.keymap.set("n", "<leader>u", "<cmd>undolist<CR>", { desc = "Show undo list" })
 
 -- Search TODO/FIXME/HACK (replaces todo-comments)
-vim.keymap.set("n", "<leader>ft", function()
-  require("fzf-lua").grep({ search = "TODO\\|FIXME\\|HACK\\|NOTE" })
-end, { desc = "[F]ind [T]ODOs" })
-vim.keymap.set("n", "<space>e", function()
-  vim.ui.input({}, function(c) 
-      if c and c~="" then 
-        vim.cmd("noswapfile vnew") 
-        vim.bo.buftype = "nofile"
-        vim.bo.bufhidden = "wipe"
-        vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.fn.systemlist(c))
-      end 
-  end) 
-end)
+-- vim.keymap.set("n", "<leader>ft", function()
+  -- require("fzf-lua").grep({ search = "TODO\\|FIXME\\|HACK\\|NOTE" })
+-- end, { desc = "[F]ind [T]ODOs" })
+-- vim.keymap.set("n", "<space>e", function()
+  -- vim.ui.input({}, function(c) 
+      -- if c and c~="" then 
+        -- vim.cmd("noswapfile vnew") 
+        -- vim.bo.buftype = "nofile"
+        -- vim.bo.bufhidden = "wipe"
+        -- vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.fn.systemlist(c))
+      -- end 
+  -- end) 
+-- end)
